123d80603690bbabd5d5c38e274e51260e46959a
kubabukry <95041821+kubabukry@users.noreply.github.com>
2023-12-26 23:33:52 +0100

    saving state for text input

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -20,7 +20,15 @@ export default function Meme(){
         randomImage: url
       }))
   }
-  
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        [name]: value
+    }))
+}
+
     return (
     <main className="meme">
       <div className='form'>
@@ -28,15 +36,25 @@ export default function Meme(){
             className='form--input' 
             type="text"
             placeholder='Top text'
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
         />
         <input 
             className='form--input' 
             type="text" 
             placeholder='Bottom text'
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
         />
         <button onClick={getMemeImage} className='form--button' type="submit">Get a new meme image ✨</button>
       </div>
+      <div className='meme'>
       <img className='meme--image' src={meme.randomImage} alt='random meme' />
+      <h2 className="meme--text top">{meme.topText}</h2>
+      <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   )
 };
